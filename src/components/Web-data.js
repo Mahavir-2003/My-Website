8da45d134db2ec0c3a92e9f452a37de5ba38731e
Mahavir Patel <patelmahavir20102003@gmail.com>
2022-08-23 17:15:59 +0530

    adding dist

--- src/components/Web-data.js
+++
@@ -1,9 +0,0 @@
-
-const MyBio = ["Hey folks, myself Mahavir Patel." , "I am a B.Tech student of Computer Engineering and a passionate coder as well as a full-stack developer.", "I have worked over 10+ open source as well as freelance projects.","Apart from that, I am a tech geek curious to learn new technologies and tools of development and other fields" , "such as blockchain-development, software development and android development."];
-
-const technical = ["My strengths are in HTML5, CSS3, JavaScript and React." ,"Other than that, I am also versatile with other front-end frameworks and back-end as well.","At last, I can also be a good fit for MERN Stack developer position."];
-const nontechnical = ["Apart from technical stuff, I am a good presentator as well as a good communicatior." ," I am also exposed to out door sports like Cricket and Football and Indoor sports like Carom and Chess."];
-const university = ["Pursuing B.Tech in Computer Engineering at Ganpat University." ,"Currently studing in 3rd sem of the course."];
-
-
-export {MyBio , technical, nontechnical, university};
